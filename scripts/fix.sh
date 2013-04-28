aaf383eb6e75a9759758c884b34a211e9fa48d06
Arne Roomann-Kurrik <kurrik@gmail.com>
2013-04-28 13:27:59 -0700

    Fix libvorbis too

--- scripts/fix.sh
+++ scripts/fix.sh
@@ -10,5 +10,6 @@ install_name_tool -change /usr/local/lib/libSDL_mixer-1.2.0.dylib @executable_pa
 install_name_tool -change /usr/local/lib/libSDL_mixer-1.2.0.dylib @executable_path/libSDL_mixer-1.2.0.dylib libSDL_mixer-1.2.0.dylib
 install_name_tool -change /usr/local/Cellar/libvorbis/1.3.3/lib/libvorbis.0.dylib @executable_path/libvorbis.0.dylib libvorbisfile.dylib
 install_name_tool -change /usr/local/lib/libogg.0.dylib @executable_path/libogg.0.dylib libvorbisfile.dylib
+install_name_tool -change /usr/local/lib/libogg.0.dylib @executable_path/libogg.0.dylib libvorbis.dylib
 chmod -w *.dylib
 otool -L ld26 libSDL*.dylib
