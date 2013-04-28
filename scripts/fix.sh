01ae81f9af7348c1a09fb4423ae33003ff77a09a
Arne Roomann-Kurrik <kurrik@gmail.com>
2013-04-28 03:36:39 -0700

    More library wrangling.

--- scripts/fix.sh
+++ scripts/fix.sh
@@ -1,3 +1,5 @@
 install_name_tool -change /usr/lib/libGLEW.1.9.0.dylib @executable_path/libGLEW.1.9.0.dylib ld26
-install_name_tool -change /usr/lib/libSDL_image-1.2.0.dylib @executable_path/libSDL_image-1.2.0.dylib ld26
+install_name_tool -change /usr/local/lib/libSDL-1.2.0.dylib @executable_path/libSDL-1.2.0.dylib ld26
+install_name_tool -change /usr/local/lib/libSDL_image-1.2.0.dylib @executable_path/libSDL_image-1.2.0.dylib ld26
+install_name_tool -change /usr/local/lib/libSDL_mixer-1.2.0.dylib @executable_path/libSDL_mixer-1.2.0.dylib ld26
 otool -L ld26
