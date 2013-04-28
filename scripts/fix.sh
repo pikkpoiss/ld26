9648e59788458a05ae059407e2d5bf7b6c3319fc
Arne Roomann-Kurrik <kurrik@gmail.com>
2013-04-28 10:03:12 -0700

    Aggressively checking in libvorbis libs.

--- scripts/fix.sh
+++ scripts/fix.sh
@@ -1,5 +1,13 @@
+chmod +w *.dylib
 install_name_tool -change /usr/lib/libGLEW.1.9.0.dylib @executable_path/libGLEW.1.9.0.dylib ld26
 install_name_tool -change /usr/local/lib/libSDL-1.2.0.dylib @executable_path/libSDL-1.2.0.dylib ld26
+install_name_tool -change /usr/local/lib/libSDL-1.2.0.dylib @executable_path/libSDL-1.2.0.dylib libSDL-1.2.0.dylib
+install_name_tool -change /usr/local/lib/libSDL-1.2.0.dylib @executable_path/libSDL-1.2.0.dylib libSDL_mixer-1.2.0.dylib
+install_name_tool -change /usr/local/lib/libSDL-1.2.0.dylib @executable_path/libSDL-1.2.0.dylib libSDL_image-1.2.0.dylib
 install_name_tool -change /usr/local/lib/libSDL_image-1.2.0.dylib @executable_path/libSDL_image-1.2.0.dylib ld26
+install_name_tool -change /usr/local/lib/libSDL_image-1.2.0.dylib @executable_path/libSDL_image-1.2.0.dylib libSDL_image-1.2.0.dylib
 install_name_tool -change /usr/local/lib/libSDL_mixer-1.2.0.dylib @executable_path/libSDL_mixer-1.2.0.dylib ld26
-otool -L ld26
+install_name_tool -change /usr/local/lib/libSDL_mixer-1.2.0.dylib @executable_path/libSDL_mixer-1.2.0.dylib libSDL_mixer-1.2.0.dylib
+install_name_tool -change /usr/local/Cellar/libvorbis/1.3.3/lib/libvorbis.0.dylib @executable_path/libvorbis.0.dylib libvorbisfile.dylib
+chmod -w *.dylib
+otool -L ld26 libSDL*.dylib
