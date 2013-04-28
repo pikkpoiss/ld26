41dbdf8a24cfe156155208281950b0e567207cc5
Arne Roomann-Kurrik <kurrik@gmail.com>
2013-04-28 03:27:17 -0700

    Fixing build script to work with SDL.

--- scripts/fix.sh
+++ scripts/fix.sh
@@ -1,3 +1,3 @@
 install_name_tool -change /usr/lib/libGLEW.1.9.0.dylib @executable_path/libGLEW.1.9.0.dylib ld26
-#install_name_tool -change @executable_path/libglfw.dylib @executable_path/libglfw.dylib ld26
+install_name_tool -change /usr/lib/libSDL_image-1.2.0.dylib @executable_path/libSDL_image-1.2.0.dylib ld26
 otool -L ld26
