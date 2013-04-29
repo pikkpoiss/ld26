7f5bf6e82b5d1e824703483ac32d33cb48d4668e
Arne Roomann-Kurrik <kurrik@gmail.com>
2013-04-29 01:03:02 -0700

    Fix libogg breakage.

--- scripts/fix.sh
+++ scripts/fix.sh
@@ -9,7 +9,14 @@ install_name_tool -change /usr/local/lib/libSDL_image-1.2.0.dylib @executable_pa
 install_name_tool -change /usr/local/lib/libSDL_mixer-1.2.0.dylib @executable_path/libSDL_mixer-1.2.0.dylib moonshot
 install_name_tool -change /usr/local/lib/libSDL_mixer-1.2.0.dylib @executable_path/libSDL_mixer-1.2.0.dylib libSDL_mixer-1.2.0.dylib
 install_name_tool -change /usr/local/Cellar/libvorbis/1.3.3/lib/libvorbis.0.dylib @executable_path/libvorbis.0.dylib libvorbisfile.dylib
+install_name_tool -change /usr/local/Cellar/libvorbis/1.3.3/lib/libvorbis.0.dylib @executable_path/libvorbis.0.dylib libvorbisfile.3.dylib
+install_name_tool -change /usr/local/lib/libvorbisfile.3.dylib @executable_path/libvorbisfile.3.dylib libvorbisfile.dylib
+install_name_tool -change /usr/local/lib/libvorbisfile.3.dylib @executable_path/libvorbisfile.3.dylib libvorbisfile.3.dylib
 install_name_tool -change /usr/local/lib/libogg.0.dylib @executable_path/libogg.0.dylib libvorbisfile.dylib
+install_name_tool -change /usr/local/lib/libogg.0.dylib @executable_path/libogg.0.dylib libvorbisfile.3.dylib
 install_name_tool -change /usr/local/lib/libogg.0.dylib @executable_path/libogg.0.dylib libvorbis.dylib
+install_name_tool -change /usr/local/lib/libogg.0.dylib @executable_path/libogg.0.dylib libvorbis.0.dylib
+install_name_tool -change /usr/local/lib/libvorbis.0.dylib @executable_path/libvorbis.0.dylib libvorbis.dylib
+install_name_tool -change /usr/local/lib/libvorbis.0.dylib @executable_path/libvorbis.0.dylib libvorbis.0.dylib
 chmod -w *.dylib
-otool -L moonshot libSDL*.dylib
+otool -L moonshot libSDL*.dylib libvorbis*
